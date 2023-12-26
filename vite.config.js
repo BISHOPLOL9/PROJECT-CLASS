0de3e56c027439edc90aabc60ec4c1847b4df2d0
BISHOPLOL9 <154857447+BISHOPLOL9@users.noreply.github.com>
2023-12-26 09:44:36 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
     host: '0.0.0.0',
-    hmr: true, // Change this line to false disable auto-refreshing.
+    hmr: true,
+  },
+  build: {
+    // If your index.html file is in the root, you don't need to include the `input` option
+    // If it's elsewhere, you should specify the path like so:
+     rollupOptions: {
+       input: '/workspaces/PROJECT-CLASS/project-root/index.html',
+     }
   }
-})
+});
